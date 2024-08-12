2e4028e0dbe2b8b716bc8280a0cd1faebdcd6afa
Democes <mostyn.jefferson@gmail.com>
2024-08-12 13:57:06 +0100

    s3 buckets added, gitignore updated

---
+++ terraform/vars.tf
@@ -0,0 +1,10 @@
+variable "ingestion_bucket_prefix" {
+  type    = string
+  default = "de-rossolimo-ingestion"
+}
+
+
+variable "code_bucket_prefix" {
+  type    = string
+  default = "de-rossolimo-code"
+}
