6f9ef2a408f610ddd8c70d09c936cd066a738337
MichaelConnolly98 <connollym98@gmail.com>
2024-08-12 16:08:40 +0100

    refactored extract

--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,17 @@
-from db_connection import connection
+from get_db_credentials import get_db_credentials as credentials_dict
+from pg8000.native import Connection
+
+def get_connection():
+    return Connection(
+        user=credentials_dict["username"],
+        password=credentials_dict["password"],
+        database=credentials_dict["database"],
+        host=credentials_dict["host"],
+        port=credentials_dict["port"]
+        )
+
 
 """extracts data from database"""
 
 def lambda_handler(event, context):
-    
+    pass
