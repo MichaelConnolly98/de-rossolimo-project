ec80f2904118ef75820f26bf591854832af68de5
MichaelConnolly98 <connollym98@gmail.com>
2024-08-12 11:42:02 +0100

    created terraform main

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "de-team-rossolimo-terraform-state"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      ProjectName = "de-team-rossolimo-project"
+      Team = "Rossolimo"
+      DeployedFrom = "Terraform"
+      Repository = "de-rossolimo-project"
+      Environment = "dev"
+      TeamMembers = "Michael, Mostyn, Nick, Leonette, Heiman"
+    }
+  }
+}
