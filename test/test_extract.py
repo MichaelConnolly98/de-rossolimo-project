a980b492db4caabe6dce5f373a606d53f5b2d4ae
diasleonette <diasleonette@gmail.com>
2024-08-13 15:23:49 +0100

    begins tests for extract function

---
+++ test/test_extract.py
@@ -0,0 +1,12 @@
+from src.extract import extract
+import pytest
+
+
+def test_extract_function_returns_a_dictionary():
+    test_func = extract()
+    assert isinstance(test_func, dict)
+    assert list(test_func.keys()) == ["all_data"]
+
+def test_extract_function_returns_expected_number_of_tables():
+    test_func = extract()
+    assert len(test_func["all_data"]) == 11
