f212a4df3228cc5e806718c01f83eac1b8b0e388
MichaelConnolly98 <connollym98@gmail.com>
2024-08-13 12:08:43 +0100

    finished testing for load function

--- src/load.py
+++ src/load.py
@@ -7,22 +7,26 @@ import logging
 logger = logging.getLogger("MyLogger")
 logger.setLevel(logging.INFO)
 
-""" writes date to date encoded s3 folder split into table folders """
+""" writes data to date encoded s3 folder split into table folders """
+
+
 def load(data):
+
     s3 = boto3.client('s3')
     BUCKETNAME = os.environ['S3_BUCKET_NAME']
+    date = datetime.now()
     folder_name = datetime.now().strftime("%Y-%m-%d")
     folder_name_2 = datetime.now().strftime('%H:%M:%S')
 
     try: 
         s3.put_object(
             Bucket = (BUCKETNAME),
-            Key = (folder_name + '/' + folder_name_2),
+            Key = (f'year={date.year}/month={date.month}/day={date.day}/{folder_name_2}'),
             Body = json.dumps(data)
         )
         logger.info(f'success at {folder_name} {folder_name_2}')
         return {'result': 'success'}
     except Exception as e:
-        logger.info(f'error occurred while trying to upload to s3 bucket')
+        logger.error(f'error occurred while trying to upload to s3 bucket')
         return e
         
--- test/test_load.py
+++ test/test_load.py
@@ -4,7 +4,8 @@ import pytest
 import boto3
 import os
 import logging
-from unittest.mock import patch
+from unittest.mock import patch, Mock
+
 
 logger = logging.getLogger('test')
 logger.setLevel(logging.INFO)
@@ -28,13 +29,21 @@ def s3_client(aws_creds):
         yield s3
 
 def test_func_loads_object_and_logs(s3_client, caplog):
-    assert load({"fake_column": "fake_data"}) == {'result': 'success'}
     with caplog.at_level(logging.INFO):
-        load({'fake': 'Data'})
+        assert load({'fake': 'Data'}) == {'result': 'success'}
         assert 'success' in caplog.text
 
-@patch('src.load.BUCKETNAME', side_effect='fake_bucket')
+@patch('src.load.boto3.client', side_effect=Exception)
 def test_func_raises_exception_and_logs(s3_client, caplog):
     with caplog.at_level(logging.INFO):
-        assert load(data=None) == Exception
-        assert 'error' in caplog.text
+        with pytest.raises(Exception):
+            load(123)
+            assert 'error' in caplog.text
+
+@patch('src.load.datetime')
+def test_func_logs_correct_time( datetime_patch, s3_client, caplog):
+    datetime_patch.now().return_value = '2002-11-09T16:38:23.417667'
+    datetime_patch.now.return_value.strftime.side_effect = ['2002-11-09', '16:38:23']
+    with caplog.at_level(logging.INFO):
+        load(123)
+        assert '2002-11-09' in caplog.text
