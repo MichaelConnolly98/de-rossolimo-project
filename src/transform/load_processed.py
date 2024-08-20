829b011efb69d8a964836c29196b1f45928fe86f
MichaelConnolly98 <connollym98@gmail.com>
2024-08-20 12:37:59 +0100

    psuedocode for writing function

--- src/transform/load_processed.py
+++ src/transform/load_processed.py
@@ -1,6 +1,13 @@
 "placeholder so not empty"
 import pandas as pd
+import boto3
+
+
+# takes dataframe, puts it into parquet, stores as buffer, then saves buffer to s3 bucket
+# buffer stops need to save locally
 
 def load_processed(df):
-    pq = pd.DataFrame.to_parquet(df)
-    return pq
+    s3 = boto3.client('s3')
+    # s3.put_object
+    # df.to_parquet()
+    # return pq
--- test/test_transform/test_load_processed.py
+++ test/test_transform/test_load_processed.py
@@ -2,7 +2,33 @@ from src.transform.load_processed import load_processed
 from src.transform.pandas_testing import dataframe_creator
 import json
 import pandas as pd
+from moto import mock_aws
+import boto3
+import pytest
+import os
+
+@pytest.fixture()
+def aws_creds():
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+    os.environ["S3_BUCKET_NAME"] = "test-bucket"
+
+
+@pytest.fixture()
+def s3_client(aws_creds):
+    with mock_aws():
+        s3 = boto3.client("s3")
+        s3.create_bucket(
+            Bucket="test-bucket",
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        yield s3
+
 
 def test_func_transforms_to_parquet():
     dataf = dataframe_creator('address')
-    print(load_processed(dataf))
+    result = load_processed(dataf)
+    
