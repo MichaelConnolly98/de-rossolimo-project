e375167aa51accb0080c7bfb98116d73a27129e5
Nicholas Slocombe <nicholassloc@aol.com>
2024-08-20 14:16:49 +0100

    add function to get currency name from currency code

---
+++ test/test_currency_to_name.py
@@ -0,0 +1,10 @@
+from src.transform.currency_code_to_name import currency_code_to_name
+import pytest
+
+def test_valid_currency_code_returns_name():
+    name = currency_code_to_name('USD')
+    assert name == 'US Dollar'
+
+def test_invalid_currency_code_returns_KeyError()):
+    with pytest.raises(KeyError):
+        name = currency_code_to_name('does_not_exist')
