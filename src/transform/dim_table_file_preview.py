ba84ed108b105ab9797acebd96129cdaa62655cc
Democes <mostyn.jefferson@gmail.com>
2024-08-20 09:21:22 +0100

    add some files for help with dataframe creation

---
+++ src/transform/dim_table_file_preview.py
@@ -0,0 +1,47 @@
+from pandas_testing import dataframe_creator
+import pandas as pd
+import iso4217parse
+#might be that you want to be more specific - use drop method to remove certain columns by name 
+#not by index reference
+#use the id column from the table it's drawing from 
+#need to change the indexes
+
+
+def staff_dim():
+    part_staff_df = dataframe_creator(table_name="staff")
+    department_df = dataframe_creator(table_name="department")
+    full_staff_df = pd.merge(part_staff_df, department_df[["department_id", "department_name", "location"]], on="department_id", how="left")
+    #reorder columns
+    cols = full_staff_df.columns.tolist()
+    cols = cols[0:3] + cols[7:] + cols[4:5]
+    full_staff_df = full_staff_df[cols]
+    return full_staff_df
+
+print(staff_dim())
+
+def payment_dim():
+    payment_df = dataframe_creator(table_name="payment_type")
+    payment_df = payment_df.iloc[:,0:2]
+    return payment_df
+
+def counterparty_dim():
+    part_counterparty_df = dataframe_creator(table_name="counterparty")
+    address_df = dataframe_creator(table_name="address")
+    full_counterparty_df = pd.merge(part_counterparty_df, address_df, left_on="legal_address_id",right_on="address_id", how="left")
+    cols = full_counterparty_df.columns.tolist()
+    cols = cols[0:2] + cols[8:15]
+    full_counterparty_df = full_counterparty_df[cols]
+    #fair few nones for address lines here, probably fine
+    return counterparty_dim
+
+def currency_name_maker(currency):
+    """
+    Takes a currency code and converts to a currency name
+    """
+    return iso4217parse.parse(currency)[0].name
+
+def currency_dim():
+    currency_df = dataframe_creator(table_name="currency")
+    currency_df = currency_df.iloc[:, 0:2]
+    currency_df["currency_name"] = currency_df["currency_code"].apply(currency_name_maker)
+    return currency_df
---
+++ test/test_transform/test_create_date_table.py
@@ -0,0 +1,27 @@
+from pandas_date_table_practice import create_date_table
+import pandas as pd
+
+def test_create_date_table_returns_dataframe():
+    result = create_date_table()
+    assert isinstance(result, pd.DataFrame)
+
+def test_create_date_table_has_required_columns():
+    result = create_date_table()
+    print(result.columns)
+    for el in [
+    "year", "month", "day", "day_of_week", "day_name", "month_name", "quarter"
+        ]:
+        assert el in result.columns
+
+def test_create_date_table_has_expected_value_for_year_rows():
+    result = create_date_table()
+    for el in [
+    "year", "month", "day", "day_of_week", "day_name", "month_name", "quarter"
+        ]:
+        col = result.loc[:, el]
+        if col.name == "year":
+            for i in range(2000, 2051):
+                assert i in col.values
+        
+                
+        
