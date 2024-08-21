11445b83c69937181badde150b6199411e15bc9a
Democes <mostyn.jefferson@gmail.com>
2024-08-21 11:16:45 +0100

    rearrange dim and fact generation functions and test suite. create sales fact generator

---
+++ src/transform/dim_generator.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from src.transform.currency_code_to_name import currency_code_to_name
+from src.transform.pandas_testing import dataframe_creator
+
+def create_date_table(
+        start='2000-01-01',
+          end='2050-12-31'
+    ):
+    """
+    Creates a date dimension table that should not need to be modified
+    after creation
+
+    Should only be ran once to generate original table
+    """
+   
+    df = pd.DataFrame(
+        {"Date": pd.date_range(start, end)}
+        )
+    df["year"] = df.Date.dt.year
+    df["month"] = df.Date.dt.month
+    df["day"] = df.Date.dt.day
+    df["day_of_week"] = df.Date.dt.day_of_week
+    df["day_name"] = df.Date.dt.day_name()
+    df["month_name"] = df.Date.dt.month_name()
+    df["quarter"] = df.Date.dt.quarter
+    df.set_index("Date", inplace=True)
+    df.index.name="date_id"
+    return df
+
+print(create_date_table())
+
+
+def currency_dim(currency_df):
+    currency_df = currency_df.drop(['created_at', 'last_updated'], axis=1)
+    currency_df["currency_name"] = currency_df["currency_code"].apply(currency_code_to_name)
+    return currency_df
+
+def payment_type_dim(currency_df):
+    currency_df = currency_df.drop(['created_at', 'last_updated'], axis=1)
+    return currency_df
+
+def staff_dim():
+    part_staff_df = dataframe_creator(table_name="staff")
+    department_df = dataframe_creator(table_name="department")
+
+    full_staff_df = part_staff_df.join(
+        department_df, on="department_id", how="left", rsuffix="a"
+        )
+    desired_columns_and_order = [
+        "first_name", "last_name", "department_name", "location", "email_address"
+        ]
+    full_staff_df = full_staff_df[desired_columns_and_order]
+    return full_staff_df
+
+def counterparty_dim():
+    part_counterparty_df = dataframe_creator(table_name="counterparty")
+    address_df = dataframe_creator(table_name="address")
+    address_df["legal_address_id"] = address_df.index
+    
+
+    full_counterparty_df = part_counterparty_df.join(
+        address_df, on="legal_address_id", how="left", rsuffix="a")
+    desired_columns_and_order = [
+        "counterparty_legal_name", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"
+    ]
+    
+    
+    full_counterparty_df = full_counterparty_df[desired_columns_and_order]
+    full_counterparty_df.rename(columns={
+        "address_line_1" : "counterparty_legal_address_line_1",
+        "address_line_2" : "counterparty_legal_address_line_2",
+        "district" : "counterparty_legal_district",
+        "city" : "counterparty_legal_city",
+        "postal_code" : "counterparty_legal_postal_code",
+        "country": "counterparty_legal_country",
+        "phone" : "counterparty_legal_phone_number"
+    }, inplace=True)
+    return full_counterparty_df
+
+
---
+++ test/test_transform/test_dim_generators.py
@@ -0,0 +1,160 @@
+from src.transform.dim_generator import create_date_table
+import pandas as pd
+from src.transform.dim_generator import currency_dim
+from src.transform.read_ingestion import read_ingestion
+from src.transform.dim_generator import counterparty_dim
+from src.transform.dim_generator import payment_type_dim
+from src.transform.dim_generator import staff_dim
+
+class TestCreateDate:
+    def test_create_date_table_returns_dataframe(self):
+        result = create_date_table()
+        assert isinstance(result, pd.DataFrame)
+
+    def test_create_date_table_has_required_columns(self):
+        result = create_date_table()
+        for el in [
+        "year", "month", "day", "day_of_week", "day_name", "month_name", "quarter"
+            ]:
+            assert el in result.columns
+
+    def test_create_date_table_has_expected_value_for_year_rows(self):
+        result = create_date_table()
+        for el in [
+        "year", "month", "day", "day_of_week", "day_name", "month_name", "quarter"
+            ]:
+            col = result.loc[:, el]
+            if col.name == "year":
+                for i in range(2000, 2051):
+                    assert i in col.values
+            if col.name == "month":
+                for i in range(1, 13):
+                    assert i in col.values
+            if col.name == "day":
+                for i in range(1, 5):
+                    assert i in col.values
+                    
+    def test_data_types_are_expected(self):
+        result = create_date_table()
+        assert result["year"].dtype == "int32"
+        assert result["month"].dtype == "int32"
+        assert result["day"].dtype == "int32"
+        assert result["day_of_week"].dtype == "int32"
+        assert result["day_name"].dtype == "object"
+        assert result["month_name"].dtype == "object"
+        assert result["quarter"].dtype == "int32"
+            
+class TestCurrencyDim:
+
+   
+    def test_currency_dim_returns_data_frame(self):
+        dataframes = read_ingestion()
+        response_df = currency_dim(dataframes['currency'])
+        assert type(response_df) == pd.core.frame.DataFrame
+
+    def test_currency_dim_returns_correct_columns(self):
+        dataframes = read_ingestion()
+        response_df = currency_dim(dataframes['currency'])
+        expected_columns = ['currency_code', 'currency_name']
+        response_columns = list(response_df.columns.values)
+        print(response_df.index)
+        for column in expected_columns:
+            assert column in response_columns
+        assert response_df.index.name=='currency_id'
+        assert len(response_columns) == len(expected_columns)
+
+
+    def test_currency_dim_returns_rows_with_correct_data_types(self):
+        dataframes = read_ingestion()
+        response_df = currency_dim(dataframes['currency'])
+        columns = ['currency_code', 'currency_name']
+        for coulumn in columns:
+            assert response_df.dtypes[coulumn] == object
+
+class TestDimCounterparty:
+
+
+    def test_counterparty_dim_returns_dataframe(self):
+        result = counterparty_dim()
+        assert isinstance(result, pd.DataFrame)
+
+    def test_counterparty_dim_has_required_columns(self):
+        result = counterparty_dim()
+        for el in [
+        "counterparty_legal_address_line_1",
+        "counterparty_legal_address_line_2",
+        "counterparty_legal_district", 
+        "counterparty_legal_city", 
+        "counterparty_legal_country",
+        "counterparty_legal_phone_number",
+        "counterparty_legal_postal_code"
+            ]:
+            assert el in result.columns
+                    
+    def test_counterparty_data_types_are_expected(self):
+        result = counterparty_dim()
+        assert result["counterparty_legal_address_line_2"].dtype == "object"
+        assert result["counterparty_legal_address_line_1"].dtype == "object"
+        assert result["counterparty_legal_district"].dtype == "object"
+        assert result["counterparty_legal_city"].dtype == "object"
+        assert result["counterparty_legal_country"].dtype == "object"
+        assert result["counterparty_legal_phone_number"].dtype == "object"
+        assert result["counterparty_legal_postal_code"].dtype == "object"
+
+    def test_counterparty_index_is_expected(self):
+        result = counterparty_dim()
+        assert result.index.name == "counterparty_id"
+
+class TestDimPayment:
+
+
+    def test_payment_type_dim_returns_data_frame(self):
+        dataframes = read_ingestion()
+        response_df = payment_type_dim(dataframes['payment_type'])
+        assert type(response_df) == pd.core.frame.DataFrame
+
+    def test_payment_type_dim_returns_correct_columns(self):
+        dataframes = read_ingestion()
+        response_df = payment_type_dim(dataframes['payment_type'])
+        expected_columns = ['payment_type_name']
+        reponse_columns = list(response_df.columns.values)
+        print(response_df.index)
+        assert len(expected_columns) == len(reponse_columns)
+        for column in expected_columns:
+            assert column in reponse_columns
+        assert response_df.index.name=='payment_type_id'
+
+    def test_payment_type_dim_returns_rows_with_correct_data_types(self):
+        dataframes = read_ingestion()
+        response_df = payment_type_dim(dataframes['payment_type'])
+        columns = ['payment_type_name']
+        for coulumn in columns:
+            assert response_df.dtypes[coulumn] == object
+
+class TestDimStaff:
+
+
+    def test_staff_dim_returns_dataframe(self):
+        result = staff_dim()
+        assert isinstance(result, pd.DataFrame)
+
+    def test_staff_dim_has_required_columns(self):
+        result = staff_dim()
+        for el in [
+        "first_name", "last_name", "department_name", "location", "email_address"
+            ]:
+            assert el in result.columns
+                    
+    def test_staff_dim_data_types_are_expected(self):
+        result = staff_dim()
+        assert result["first_name"].dtype == "object"
+        assert result["last_name"].dtype == "object"
+        assert result["department_name"].dtype == "object"
+        assert result["location"].dtype == "object"
+        assert result["email_address"].dtype == "object"
+
+    def test_staff_dim_index_is_expected(self):
+        result = staff_dim()
+        assert result.index.name == "staff_id"
+
+            
