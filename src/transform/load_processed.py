3185be0d22585325f165ad43467426a0a44ae3e3
MichaelConnolly98 <connollym98@gmail.com>
2024-08-21 16:46:58 +0100

    complete testing suite for load_processed

--- src/transform/load_processed.py
+++ src/transform/load_processed.py
@@ -13,25 +13,34 @@ logger.setLevel(logging.INFO)
 # buffer stops need to save locally problems might arise if data is longer than memory
 # - look out for this !!
 
+
 def load_processed(df):
     try:
-        table_name = df.name
         date = datetime.now()
         folder_name = datetime.now().strftime("%Y-%m-%d")
         folder_name_2 = datetime.now().strftime("%H:%M:%S")
-        #aws client that connects to s3
-        s3 = boto3.client('s3')
-        #temperary place to store bytes of parquet
-        out_buffer = BytesIO()  
-        # put parquet into temp store
-        df.to_parquet(out_buffer)
-        #upload to s3 bucketname is placeholder for now
-        s3.put_object(
-            Bucket = 'test-bucket',
-            Key = f"table={table_name}/year={date.year}/month={date.month}/day={date.day}/{folder_name_2}.parquet",
-            Body = out_buffer.getvalue()
-        )
-        # return value is logged?
+        if df is not None:
+            table_name = df.name
+            
+            #aws client that connects to s3
+            s3 = boto3.client('s3')
+            #temperary place to store bytes of parquet
+            out_buffer = BytesIO()  
+            # put parquet into temp store
+            df.to_parquet(out_buffer)
+            #upload to s3 bucketname is placeholder for now
+            s3.put_object(
+                Bucket = 'test-bucket',
+                Key = f"table={table_name}/year={date.year}/month={date.month}/day={date.day}/{folder_name_2}.parquet",
+                Body = out_buffer.getvalue()
+            )
+            # return value is logged?
+            logger.info({"Result": "Success", "Message": f"data uploaded at {folder_name} {folder_name_2}"})
+            return {"Result": "Success", "Message": "data uploaded"}
+
+        else:
+            logger.info({"Message": f"no data to upload at {folder_name} {folder_name_2}"})
+            return {"Message": "no data to upload"}
     except AttributeError as ae:
         logger.error({"Result": "Failure", "Error": f"AttributeError occurred: {str(ae)}"})
         raise ae 
