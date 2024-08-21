8909881a2eb7e6ecbd1132f792f7a417e8bcb46f
Nicholas Slocombe <nicholassloc@aol.com>
2024-08-21 14:48:25 +0100

    complete purchase_order_facts

--- src/transform/fact_generator.py
+++ src/transform/fact_generator.py
@@ -45,8 +45,44 @@ def sales_facts():
 
     return sales_order
 
+def purchase_order_facts():
+    purchase_order = dataframe_creator('purchase_order')
+
+    purchase_order["created_date"] = pd.to_datetime(pd.to_datetime(purchase_order["created_at"], format="mixed").dt.date)
+    purchase_order["created_time"] = pd.to_datetime(purchase_order["created_at"], format="mixed").dt.time
+    purchase_order.drop(["created_at"], axis=1, inplace=True)
+
+    purchase_order["last_updated_date"] = pd.to_datetime(pd.to_datetime(purchase_order["last_updated"], format="mixed").dt.date)
+    purchase_order["last_updated_time"] = pd.to_datetime(purchase_order["last_updated"], format="mixed").dt.time
+    purchase_order.drop(["last_updated"], axis=1, inplace=True)
+
+    purchase_order["agreed_payment_date"] = pd.to_datetime(purchase_order["agreed_payment_date"])
+    purchase_order["agreed_delivery_date"] = pd.to_datetime(purchase_order["agreed_delivery_date"])
+
+    purchase_order['item_unit_price'] = pd.to_numeric(purchase_order['item_unit_price'])
+
+    purchase_order["purchase_order_id"] = purchase_order.index
+    purchase_order.index.name = "purchase_record_id"
+
+    desired_order = [
+        "purchase_order_id", 
+        "created_date", 
+        "created_time", 
+        "last_updated_date", 
+        "last_updated_time", 
+        "staff_id", 
+        "counterparty_id", 
+        "item_code", 
+        "item_quantity", 
+        "item_unit_price", 
+        "currency_id", 
+        "agreed_delivery_date",
+        "agreed_payment_date", 
+        "agreed_delivery_location_id"
+    ]
+
+    purchase_order = purchase_order[desired_order]
+    purchase_order.name = "fact_purchase_order"
 
 
-
-
-
+    return purchase_order
--- test/test_transform/test_fact_generator.py
+++ test/test_transform/test_fact_generator.py
@@ -1,12 +1,12 @@
-from src.transform.fact_generator import sales_facts
+from src.transform.fact_generator import sales_facts, purchase_order_facts
 import pandas as pd
 
 class TestSalesFact:
-    def test_currency_dim_returns_data_frame(self):
+    def test_sales_facts_returns_data_frame(self):
         response_df = sales_facts()
         assert type(response_df) == pd.core.frame.DataFrame
 
-    def test_currency_dim_returns_correct_columns(self):
+    def test_sales_facts_returns_correct_columns(self):
         response_df = sales_facts()
         expected_columns = [
             "sales_order_id", 
@@ -30,7 +30,7 @@ class TestSalesFact:
         assert response_df.index.name=='sales_record_id'
         assert len(response_columns) == len(expected_columns)
 
-    def test_currency_dim_returns_rows_with_correct_data_types(self):
+    def test_sales_facts_returns_rows_with_correct_data_types(self):
         response_df = sales_facts()
         assert response_df.dtypes['sales_order_id'] == 'int64'
         assert response_df.dtypes['created_date'] == '<M8[ns]'
@@ -45,4 +45,51 @@ class TestSalesFact:
         assert response_df.dtypes['design_id'] == 'int64'
         assert response_df.dtypes['agreed_payment_date'] == '<M8[ns]'
         assert response_df.dtypes['agreed_delivery_date'] == '<M8[ns]'
+        assert response_df.dtypes['agreed_delivery_location_id'] == 'int64'
+
+class TestPurchaseOrderFact:
+    def test_purchase_order_facts_returns_data_frame(self):
+        response_df = purchase_order_facts()
+        assert type(response_df) == pd.core.frame.DataFrame
+
+    def test_purchase_order_facts_returns_correct_columns(self):
+        response_df = purchase_order_facts()
+        expected_columns = [
+            "purchase_order_id", 
+            "created_date", 
+            "created_time", 
+            "last_updated_date", 
+            "last_updated_time", 
+            "staff_id", 
+            "counterparty_id", 
+            "item_code", 
+            "item_quantity", 
+            "item_unit_price", 
+            "currency_id", 
+            "agreed_delivery_date",
+            "agreed_payment_date", 
+            "agreed_delivery_location_id"
+            ]
+
+        response_columns = list(response_df.columns.values)
+        for column in expected_columns:
+            assert column in response_columns
+        assert response_df.index.name=='purchase_record_id'
+        assert len(response_columns) == len(expected_columns)
+
+    def test_purchase_order_facts_returns_rows_with_correct_data_types(self):
+        response_df = purchase_order_facts()
+        assert response_df.dtypes['purchase_order_id'] == 'int64'
+        assert response_df.dtypes['created_date'] == '<M8[ns]'
+        assert response_df.dtypes['created_time'] == object
+        assert response_df.dtypes['last_updated_date'] == '<M8[ns]'
+        assert response_df.dtypes['last_updated_time'] == object
+        assert response_df.dtypes['staff_id'] == 'int64'
+        assert response_df.dtypes['counterparty_id'] == 'int64'
+        assert response_df.dtypes['item_code'] ==  object
+        assert response_df.dtypes['item_quantity'] == 'int64'
+        assert response_df.dtypes['item_unit_price'] == 'float64'
+        assert response_df.dtypes['currency_id'] == 'int64'
+        assert response_df.dtypes['agreed_delivery_date'] == '<M8[ns]'
+        assert response_df.dtypes['agreed_payment_date'] == '<M8[ns]'
         assert response_df.dtypes['agreed_delivery_location_id'] == 'int64'
