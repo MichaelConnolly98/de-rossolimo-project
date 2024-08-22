6b56f632cbaa4dfd540101a79795836e6f412789
MichaelConnolly98 <connollym98@gmail.com>
2024-08-22 16:52:21 +0100

    first attempt at test for loading pq to db

---
+++ src/load/load_pq_to_db.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import pyarrow as pq
+
+# func that takes parquets from s3 bucket 
+# then transforms them back into df then loads into data warehouse
+
+def load_to_dw(pq_file):
+    pass
