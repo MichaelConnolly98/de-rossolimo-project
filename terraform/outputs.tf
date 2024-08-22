36fb45340b03ba53037775793a030115b92a309b
Democes <mostyn.jefferson@gmail.com>
2024-08-22 15:37:26 +0100

    change t terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,6 @@ resource "local_file" "s3_bucket_name" {
 }
 
 resource "local_file" "process_s3_bucket_name" {
-    content = aws_s3_bucket.data_bucket.bucket
+    content = aws_s3_bucket.processed_data_bucket.bucket
     filename = "${path.module}/../s3_process_bucket_name.txt"
 }
