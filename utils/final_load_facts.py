4162881b33a7b3ce6497d99be4d8091ad6e39258
Nicholas Slocombe <nicholassloc@aol.com>
2024-08-27 14:15:32 +0100

    pep8 update

--- utils/final_load_facts.py
+++ utils/final_load_facts.py
@@ -10,24 +10,15 @@ This is not yet finsihes testing, before any fact rows are inserted
 the dim tables need to be inserted first as teh facts reference the dims
 '''
 
+
 def load_facts(table_name, df):
     with get_connection('totesys_data_warehouse') as conn:
         for index, row in df.iterrows():
             columns = str(df.index.name) + ', ' + ', '.join(row.keys())
-            values = "'" + str(index) + "', '" + "', '".join(map(str, row.values)) + "'"
-            print(columns)
-            print(values)
-
-
-
-            # columns = 'sales_order_id'
-            # values = 1
-
+            values = "'" + str(index) + "', '" + "', '".join(map(
+                str, row.values)) + "'"
             sql_insert = f"""INSERT INTO {identifier(table_name)}
                         ({columns})
                         VALUES ({values});"""
             response = conn.run(sql_insert)
-            print(response)
-            break
-
-# print(get_db_credentials('totesys_data_warehouse'))
+    return response
