901fb6f17b9676c553849a4c1d624203712b9daf
Democes <mostyn.jefferson@gmail.com>
2024-08-27 18:22:34 +0100

    minor cleanup

--- utils/final_load_dims_m.py
+++ utils/final_load_dims_m.py
@@ -1,17 +1,3 @@
-import pg8000
-from pg8000.native import identifier
-from utils.extract_data import get_connection
-from utils.extract_data import get_db_credentials
-from pprint import pprint
-import pandas as pd
-import sqlalchemy
-from utils.dim_generator import location_dim
-import json
-from pg8000.exceptions import InterfaceError
-from sqlalchemy.types import Integer, String
-from utils.load_connection_dbapi import connection
-
-
 def load_dim_m(table_name, df, engine):
     result = df.to_sql(table_name, if_exists='append', con=engine, index=True)
 
